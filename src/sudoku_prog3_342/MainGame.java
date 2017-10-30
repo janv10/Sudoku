b3c057944895f397881c1ce2fda8cb3c4ef8abc7
jpate201 <jpate201@uic.edu>
2017-10-29 21:32:58 -0500

    Update MainGame.java

--- src/sudoku_prog3_342/MainGame.java
+++ src/sudoku_prog3_342/MainGame.java
@@ -2,8 +2,8 @@ package sudoku_prog3_342;
 
 import javax.swing.JFrame;
 
-public class MainGame {												//The main method creates a single instance of JFrame an 
-
+/* The main method creates a single instance of JFrame an */
+public class MainGame {												
 	public static void main(String[] args) {
 		Display guiSudoku = new Display();
 		
